0b4a5c51936553894af615e24ddb36b499de7537
Michael Whitehead <mwhitehead10@gmail.com>
2024-09-05 10:35:29 -0600

    Add a redirect for base path to /hello

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello_world():
     return 'Hello, world!'
+
+@app.route('/')
+def index():
+    return redirect(url_for('hello_world'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
